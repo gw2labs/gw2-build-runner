7fa7603505de6b275b7a751bda7776bbf248af62
Ralf Hergert <ralf.hergert@gmx.de>
2020-01-08 08:34:22 +0100

    rename ContextType into OnwerType

--- src/main/java/de/ralfhergert/generic/value/Value.java
+++ src/main/java/de/ralfhergert/generic/value/Value.java
@@ -7,15 +7,15 @@ import java.util.List;
  * This value only allows to be modified by its attached modifiers.
  *
  * @param <ValueType> type of the value itself
- * @param <ContextType> type of the object this value belongs to
+ * @param <OwnerType> type of the object this value belongs to
  */
-public class Value<ValueType,ContextType> {
+public class Value<ValueType,OwnerType> {
 
     private final String name;
     private final ValueType startValue;
 
-    private final List<ValueModifier<ValueType,ContextType>> modifiers = new ArrayList<>();
-    private final List<ValueChangedHandler<ValueType,ContextType>> changedHandlers = new ArrayList<>();
+    private final List<ValueModifier<ValueType,OwnerType>> modifiers = new ArrayList<>();
+    private final List<ValueChangedHandler<ValueType,OwnerType>> changedHandlers = new ArrayList<>();
 
     private boolean isModified = false;
     private ValueType finalValue;
@@ -30,7 +30,7 @@ public class Value<ValueType,ContextType> {
         return name;
     }
 
-    public ValueType getValue(ContextType context) {
+    public ValueType getValue(OwnerType context) {
         if (isModified) {
             finalValue = startValue;
             modifiers.forEach(modifier -> finalValue = modifier.modify(finalValue, context));
@@ -46,7 +46,7 @@ public class Value<ValueType,ContextType> {
         new ArrayList<>(changedHandlers).forEach(handler -> handler.valueHasChanged(this));
     }
 
-    private void markAsModified(ValueModifier<ValueType,ContextType> modifier) {
+    private void markAsModified(ValueModifier<ValueType,OwnerType> modifier) {
         if (modifiers.contains(modifier)) {
             isModified = true;
             /* operate on a copy of the list to avoid ConcurrentModificationException
@@ -55,25 +55,25 @@ public class Value<ValueType,ContextType> {
         }
     }
 
-    public Value<ValueType,ContextType> addModifier(ValueModifier<ValueType,ContextType> modifier) {
+    public Value<ValueType,OwnerType> addModifier(ValueModifier<ValueType,OwnerType> modifier) {
         modifiers.add(modifier);
         modifier.addChangedHandler(this::markAsModified);
         markAsModified(modifier);
         return this;
     }
 
-    public Value<ValueType,ContextType> removeModifier(ValueModifier<ValueType,ContextType> modifier) {
+    public Value<ValueType,OwnerType> removeModifier(ValueModifier<ValueType,OwnerType> modifier) {
         markAsModified(modifier);
         modifiers.remove(modifier);
         return this;
     }
 
-    public Value<ValueType,ContextType> addChangedHandler(ValueChangedHandler<ValueType,ContextType> handler) {
+    public Value<ValueType,OwnerType> addChangedHandler(ValueChangedHandler<ValueType,OwnerType> handler) {
         changedHandlers.add(handler);
         return this;
     }
 
-    public Value<ValueType,ContextType> removeChangedHandler(ValueChangedHandler<ValueType,ContextType> handler) {
+    public Value<ValueType,OwnerType> removeChangedHandler(ValueChangedHandler<ValueType,OwnerType> handler) {
         changedHandlers.remove(handler);
         return this;
     }
